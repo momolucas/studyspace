1a2ffcbd3e28fdd343402d1a1493b56ec4e51078
Lucas Silva x228477 <lucas.dos.s.silva@accenture.com>
2019-10-24 11:27:20 -0300

    [Alura] Kotlin parte 1 - Criado classe base.  Atualizada informações do manifest.  Criado classe ListaTransacoesActivit.

---
+++ Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ListaTransacoesActivity.kt
@@ -0,0 +1,12 @@
+package ssilvalucas.financas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
