7417db7fba313c4806a8252f7b7c783b4b8140bd
Lucas Silva <lucass.silva@me.com>
2019-10-27 23:08:06 -0300

    [Alura] Kotlin parte 1 - Criado adapter da lista de transacoes.

--- Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ListaTransacoesActivity.kt
+++ Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ListaTransacoesActivity.kt
@@ -2,11 +2,21 @@ package ssilvalucas.financas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import ssilvalucas.financas.ui.adapter.ListaTransacoesAdapter
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val lista = listOf("Item 1 - R$1,00", "Item 2 - R$20,00")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lista)
+
+
+        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(lista, this))
     }
 }
