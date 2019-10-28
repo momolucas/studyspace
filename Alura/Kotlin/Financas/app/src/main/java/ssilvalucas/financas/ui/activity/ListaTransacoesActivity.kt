7deb0b52c2811a5f0f2bcbf06da3fa2c5310a5ce
Lucas Silva x228477 <lucas.dos.s.silva@accenture.com>
2019-10-28 16:41:51 -0300

    [Alura] Kotlin parte 1 - Criado enum para tipo

--- Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ui/activity/ListaTransacoesActivity.kt
+++ Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ui/activity/ListaTransacoesActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 import ssilvalucas.financas.R
+import ssilvalucas.financas.model.Tipo
 import ssilvalucas.financas.model.Transacao
 import ssilvalucas.financas.ui.adapter.ListaTransacoesAdapter
 import java.math.BigDecimal
-import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
@@ -15,8 +15,8 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val lista = listOf(Transacao(BigDecimal(20.50), "comida", Calendar.getInstance()),
-            Transacao(BigDecimal(100.0), "Economia", Calendar.getInstance()))
+        val lista = listOf(Transacao(valor = BigDecimal(20.50), tipo = Tipo.DESPESA),
+            Transacao(valor = BigDecimal(100.0), categoria = "Economia", tipo = Tipo.RECEITA))
 
         lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(lista, this))
     }
