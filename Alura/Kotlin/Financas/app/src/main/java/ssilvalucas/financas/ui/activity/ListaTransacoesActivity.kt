b7e827c132ebe08c6ad0493bdc39390f65b3f639
Lucas Silva <lucass.silva@me.com>
2019-11-04 21:48:38 -0300

    [Alura] Kotlin parte 2 - Implementado dialog base.  Alterado layout form_transacao para compatibilidade.

--- Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ui/activity/ListaTransacoesActivity.kt
+++ Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ui/activity/ListaTransacoesActivity.kt
@@ -1,7 +1,11 @@
 package ssilvalucas.financas.ui.activity
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
@@ -26,7 +30,16 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         lista_transacoes_adiciona_receita
             .setOnClickListener {
-                Toast.makeText(this,"Clique de receita", Toast.LENGTH_LONG).show()
+                val view : View = window.decorView
+                val viewCriada = LayoutInflater.from(this)
+                    .inflate(R.layout.form_transacao,
+                        view as ViewGroup,
+                        false)
+
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.adiciona_receita)
+                    .setView(viewCriada)
+                    .show()
             }
     }
 
