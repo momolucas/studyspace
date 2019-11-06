f4e531fbb9a6452574f02c693e464c467c6497f4
Lucas Silva <lucass.silva@me.com>
2019-11-05 21:07:25 -0300

    [Alura] Kotlin parte 2 - Implementado DatePicker.  Esta com um bug na formatação da data.

--- Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ui/activity/ListaTransacoesActivity.kt
+++ Alura/Kotlin/Financas/app/src/main/java/ssilvalucas/financas/ui/activity/ListaTransacoesActivity.kt
@@ -1,20 +1,25 @@
 package ssilvalucas.financas.ui.activity
 
 import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import ssilvalucas.financas.R
+import ssilvalucas.financas.extension.formataParaBrasileiro
 import ssilvalucas.financas.model.Tipo
 import ssilvalucas.financas.model.Transacao
 import ssilvalucas.financas.ui.ResumoView
 import ssilvalucas.financas.ui.adapter.ListaTransacoesAdapter
 import java.math.BigDecimal
+import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
@@ -35,6 +40,27 @@ class ListaTransacoesActivity : AppCompatActivity() {
                     .inflate(R.layout.form_transacao,
                         view as ViewGroup,
                         false)
+
+                val ano = 2017
+                val mes = 9
+                val dia = 18
+
+                val hoje = Calendar.getInstance()
+
+                viewCriada.form_transacao_data
+                    .setText(hoje.formataParaBrasileiro())
+
+                viewCriada.form_transacao_data
+                    .setOnClickListener {
+                        DatePickerDialog(this,
+                            DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
+                                val dataSelecionada = Calendar.getInstance()
+                                dataSelecionada.set(ano, mes, dia)
+                                viewCriada.form_transacao_data
+                                    .setText(dataSelecionada.formataParaBrasileiro())
+                            }
+                            , ano, mes, dia).show()
+                    }
 
                 AlertDialog.Builder(this)
                     .setTitle(R.string.adiciona_receita)
